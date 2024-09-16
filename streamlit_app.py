2eef171537c7a5b5a28ad4b82fb842538ea31966
Dhanishtha Deore <101131774+Dhanishthad@users.noreply.github.com>
2024-09-16 17:00:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ with st.sidebar:
           'bill_depth_mm':bill_depth_mm,
           'flipper_length_mm':flipper_length_mm,
           'body_mass_g':body_mass_g ,
-          'gender':gender}
+          'sex':sex}
   input_df=pd.DataFrame(data,index=[0])
   input_penguins=pd.concat([input_df,X],axis=0)
 
@@ -48,8 +48,8 @@ with st.expander('Input features'):
   input_penguins
 
 # encode
-encode = ['island','gender']
+encode = ['island','sex']
 df_penguins = pd.get_dummies(input_penguins, prefix=encode)
-df_penguins[:1]
+df_penguins
 
   
