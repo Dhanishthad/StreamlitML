cc49afe8e7f15664541588af6ce1852cf4a1a906
Dhanishtha Deore <101131774+Dhanishthad@users.noreply.github.com>
2024-09-16 15:19:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import streamlit as st
 st.title('🤖 Machine Learning App')
 
 st.info('This app builds a machile learning module')
+
+df =pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+df
+
